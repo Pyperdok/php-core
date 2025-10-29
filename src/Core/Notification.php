be4e8ee5f360cfc6a3b7b0fdeb9b998c03bd38a0
Pyperdok <zhitov.egoriko@mail.ru>
2025-10-29 15:27:37 +0800

    try catch

--- src/Core/Notification.php
+++ src/Core/Notification.php
@@ -2,20 +2,29 @@
 
 namespace Tapigo\Core;
 
+use Illuminate\Http\Client\Response;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
+use Throwable;
 
 class Notification {
     const PUSH = 'PUSH';
     const EMAIL = 'EMAIL';
 
-    public static function message(string $type, array $data) {
-        $data = [
-            'type' => $type,
-            'data' => $data
-            'owner_id' => $data['owner_id'] ?? null,
-            'owner_type' => $data['owner_type'] ?? null,
-        ];
+    public static function message(string $type, array $data): Response|null {
+        try {
+            $data = [
+                'type' => $type,
+                'data' => $data
+                'owner_id' => $data['owner_id'] ?? null,
+                'owner_type' => $data['owner_type'] ?? null,
+            ];
 
-        Http::post(url('/api/notification/message'), $data);
+            return Http::post(url('/api/notification/message'), $data);
+        } catch(Throwable $th) {
+            Log::error($th->getMessage());
+
+            return null;
+        }
     }
 }
